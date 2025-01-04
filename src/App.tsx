5790d6971a382630b83ede83002fb199740a8ae1
syuwawa <kobayashiasuka.general2@gmail.com>
2025-01-04 10:11:15 +0900

    タスク追加処理

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+const[task,setTasks]=useState<string[]>([]);
+
+const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+  console.log(e.target.value);
+}
+
+//追加ボタンを押した時の処理
+const handleClick=()=>{
+  const input=document.querySelector('input');
+  if(input){
+    setTasks([...task,input.value]);
+    input.value='';
+  }
+}
+
+  return (
+    <div className="App">
+      <h2>Todo リスト</h2>
+     <input type='text' placeholder='タスク' onChange={handleChange}></input>
+     <button onClick={handleClick}>追加</button>
+    </div>
+  );
+}
+
+export default App;
