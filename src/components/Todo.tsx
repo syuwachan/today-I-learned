270b85ed1462a0bc1bc783836b3894c049397b56
syuwawa <kobayashiasuka.general2@gmail.com>
2025-01-06 15:36:07 +0900

    削除処理

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,44 +1,50 @@
-import{useState}from 'react';
-
+import { useState } from "react";
 
 function Todo() {
-	//inputに入力された値を保持するためのstate
-	const[todo,setTodo]=useState<string>('');
+  //inputに入力された値を保持するためのstate
+  const [todo, setTodo] = useState<string>("");
 
-	//todoリストを保持するためのstate
-	const[todoText,setTodoText]=useState<string[]>([]);
+  //todoリストを保持するためのstate
+  const [todoText, setTodoText] = useState<string[]>([]);
 
-	function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-		setTodo(event.target.value);
-	    }
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setTodo(event.target.value);
+  }
 
-	//todoリストにタスクを追加する関数
-	const addTask = () => {
-          if(todo===''){
-		return;
-	}else{
-		setTodoText([...todoText,todo]);
-		setTodo('');
-	}
-	}
+  //todoリストにタスクを追加する関数
+  const addTask = () => {
+    if (todo === "") {
+      return;
+    } else {
+      setTodoText([...todoText, todo]);
+      setTodo("");
+    }
+  };
 
-	return (
-	    <div className="todo">
-	        <input
-	        value={todo}
-	         onChange={handleChange}
-	         placeholder='タスク'/>
-	        <button
-	        onClick={addTask}>追加</button>
-	        <ul>
-		{todoText.map((todo,index)=>{
-			return(
-				<li key={(index)}>{todo}</li>
-			)
-		})}
-	        </ul>
-	    </div>
-	);
-      }
+  //todoリストからタスクを削除する関数
+  const deleteTask=(index:number)=>{
+  const newTodoText: string[] = [...todoText];
+  newTodoText.splice(index, 1);
+  setTodoText(newTodoText);
+  }
 
-export default Todo
+  return (
+    <div className="todo">
+      <input value={todo} onChange={handleChange} placeholder="タスク" />
+      <button onClick={addTask}>追加</button>
+
+      <ul>
+        {todoText.map((todo, index) => {
+          return (
+            <li key={index}>
+              {todo}
+              <button onClick={() => deleteTask(index)}>削除</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
